02c7adb0e4c1120b35c1cacde0ce5a5e51f3e1cb
Daisuke Nakanishi <dnaknaisi@gmail.com>
2018-02-24 14:58:58 +0900

    t.test function
    t.test function

--- sapporo.R
+++ sapporo.R
@@ -60,27 +60,8 @@ mes(result_t$estimate[1],result_t$estimate[2],result_sd[1],
 
 
 #Self defined function (22/2/2018, There may be some bugs.)
-
-daihiko_t<-function(x,y,paired=TRUE) {
-  if (paired) {
-    d<-data.frame(x,y)
-    d<-subset(d,complete.cases(d))
-    rt<-t.test(d$x,d$y,paired=TRUE)
-    rsd<-c(sd(d$x,na.rm=T),sd(d$y,na.rm=T))
-    cohend<-abs(mean(d$x-d$y)/(sd(d$x-d$y)/sqrt(2*(1-cor(d$x,d$y)))))
-    return(list(meanx=mean(d$x),meany=mean(d$y),SD=rsd,t=rt,cohend=cohend))
-  }
-  else {  
-    rt<-t.test(x~y)
-    rsd<-tapply(x,y,sd,na.rm=TRUE)
-    rt1<-table(y,x)
-    rt2<-apply(rt1,1,sum)
-    cohend<-(rt$estimate[1]-rt$estimate[2])/sqrt(((rt2[1]-1)*rsd[1]^2+(rt2[2]-1)*rsd[2]^2)
-                                                 /(rt2[1]+rt2[2]-2))
-    cohend<-unname(cohend)
-    return(list(N=rt2,SD=rsd,t=rt,cohend=cohend))
-  } 
-}
+#load daihiko_t.R
+source("daihiko_t.R")
 
 daihiko_t(d$Quiz,d$SEX,paired=FALSE)
 daihiko_t(d$FoodLiteracy01,d$FoodLiteracy02,paired=TRUE)
